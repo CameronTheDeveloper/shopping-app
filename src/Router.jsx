8a95662e8b5d492897ae33d5509062e70c2e9d6f
CameronTheDeveloper <Cameronposey@outlook.com>
2024-01-31 01:03:48 -0800

    Add HomePage & ShopPage routes

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import HomePage from "./Components/HomePage/HomePage";
+import ShopPage from "./Components/ShopPage/ShopPage";
 
 function Router() {
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <App />
+            element: <App />,
+            children: [
+                {
+                    path: "homepage/",
+                    element: <HomePage />,
+                },
+                {
+                    path: "shop/",
+                    element: <ShopPage />,
+                }
+            ],
         },
     ]);
 
