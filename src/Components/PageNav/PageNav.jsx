b2cf715dd253b33b7f70655c1a3a1dd056055c76
CameronTheDeveloper <Cameronposey@outlook.com>
2024-01-31 00:15:08 -0800

    Add PageNav component with css module

---
+++ src/Components/PageNav/PageNav.jsx
@@ -0,0 +1,12 @@
+import styles from "./PageNav.module.css";
+
+function PageNav() {
+
+    return (
+        <nav className="pageNav">
+            Nav Bar
+        </nav>
+    );
+}
+
+export default PageNav;
