d6fc94a60975fb1f68aa7e05446fbeeee3994c24
CameronTheDeveloper <Cameronposey@outlook.com>
2024-01-31 02:39:01 -0800

    Add ErrorPage error element with css module

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import "./ErrorPage.module.css";
+import { Link } from "react-router-dom";
+
+function ErrorPage() {
+    return (
+        <div>
+            <h1>Oh no, this route doesn't exist!</h1>
+            <Link to="homepage/">Return to home</Link>
+        </div>
+    );
+}
+
+export default ErrorPage;
--- src/Router.jsx
+++ src/Router.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import HomePage from "./Components/HomePage/HomePage";
 import ShopPage from "./Components/ShopPage/ShopPage";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
 
 function Router() {
     const router = createBrowserRouter([
         {
             path: "/",
             element: <App />,
+            errorElement: <ErrorPage />,
             children: [
                 {
                     path: "homepage/",
