6b0670e88c15f043eb57b91089603fd0e5301903
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-01 21:56:04 -0800

    Add HomePage welcome headers

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -3,7 +3,12 @@ import styles from "./HomePage.module.css";
 function HomePage() {
 
     return (
-        <div id="homePage">HomePage</div>
+        <div id="homePage">
+            <div>
+                <h1>Welcome to Shopping App</h1>
+                <h2>Shop in the comfort of your own home!</h2>
+            </div>
+        </div>
     );
 }
 
