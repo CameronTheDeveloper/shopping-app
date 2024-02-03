1be67f0471b75bbf866e2a0d8a57aec84378428d
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-03 10:37:13 -0800

    Add cartItemCount prop for ShoppingCart

--- src/Components/ShoppingCart/ShoppingCart.jsx
+++ src/Components/ShoppingCart/ShoppingCart.jsx
@@ -1,9 +1,16 @@
+import PropTypes from "prop-types";
 import styles from "./ShoppingCart.module.css";
 
-function ShoppingCart() {
+function ShoppingCart({ cartItemsCount = 0 }) {
     return (
-        <></>
+        <div>
+            <div>{cartItemsCount}</div>
+        </div>
     );
 }
+
+ShoppingCart.propTypes = {
+    cartItemsCount: PropTypes.number,
+};
 
 export default ShoppingCart;
