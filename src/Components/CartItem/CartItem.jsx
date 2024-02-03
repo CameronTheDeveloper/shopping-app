c7a0c4ee67a2c74f1b95611f333aa464e9d7d598
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-03 08:16:46 -0800

    Add CartItem title, price, & style

--- src/Components/CartItem/CartItem.jsx
+++ src/Components/CartItem/CartItem.jsx
@@ -1,9 +1,18 @@
-import "./CartItem.module.css";
+import PropTypes from "prop-types";
+import styles from "./CartItem.module.css";
 
-function CartItem() {
+function CartItem({ title, price }) {
     return (
-        <></>
+        <div className={styles.cartItem}>
+            <h1>{title}</h1>
+            <h2>{`$${price} USD`}</h2>
+        </div>
     );
 }
+
+CartItem.propTypes = {
+    title: PropTypes.string,
+    price: PropTypes.number,
+};
 
 export default CartItem;
