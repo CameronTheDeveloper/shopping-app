bbeedc5da346d4a5782775c17ebd4fd772aebab4
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-03 10:58:32 -0800

    Add ShoppingCart icon

--- src/Components/ShoppingCart/ShoppingCart.jsx
+++ src/Components/ShoppingCart/ShoppingCart.jsx
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
+import Icon from "@mdi/react";
+import { mdiCartVariant } from "@mdi/js";
 import styles from "./ShoppingCart.module.css";
 
 function ShoppingCart({ cartItemsCount = 0 }) {
     return (
         <div>
+            <Icon path={mdiCartVariant}
+                size={2}
+                title={"Shopping Cart"}
+            ></Icon>
             <div>{cartItemsCount}</div>
         </div>
     );
