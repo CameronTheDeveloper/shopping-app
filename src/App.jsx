61b9a47dbd0b704561ab29dd1aaf76d1e54a455b
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-05 14:44:50 -0800

    Add cartTotal state & changeCartTotal function

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import PageNav from './Components/PageNav/PageNav';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import ShoppingCart from './Components/ShoppingCart/ShoppingCart';
 import './App.css';
 
 function App() {
 
+  const [cartTotal, setCartTotal] = useState(0);
+
+  const changeCartTotal = (amount) => {
+    setCartTotal(cartTotal + amount);
+  };
+
   return (
     <>
-
       <header>
         <PageNav></PageNav>
-        <ShoppingCart></ShoppingCart>
+        <ShoppingCart cartTotal={cartTotal}></ShoppingCart>
       </header>
       <main>
         <Outlet></Outlet>
       </main>
-
     </>
   );
 }
