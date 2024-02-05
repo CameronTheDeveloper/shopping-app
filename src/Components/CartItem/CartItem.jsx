240159b88decfdd35c1255f576ff09a334a36835
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-05 13:31:06 -0800

    Add CartItem amount input

--- src/Components/CartItem/CartItem.jsx
+++ src/Components/CartItem/CartItem.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./CartItem.module.css";
 
+
 function CartItem({ title, price }) {
+    const [amount, setAmount] = useState(0);
+
+    const changeAmount = (e) => {
+        setAmount(e.target.value);
+    };
+
     return (
         <div className={styles.cartItem}>
             <h1>{title}</h1>
             <h2>{`$${price} USD`}</h2>
+            <input type="number" value={amount} onChange={changeAmount}></input>
             <button>Add To Cart</button>
         </div>
     );
