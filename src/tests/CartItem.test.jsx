af20920c1d5caef5d3413f2c6a811f9f8c93802f
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-09 13:36:14 -0800

    Add CartItem increment button test

--- src/tests/CartItem.test.jsx
+++ src/tests/CartItem.test.jsx
@@ -1,2 +1,19 @@
-import CartItem from '../Components/CartItem';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import CartItem from '../Components/CartItem/CartItem';
 
+describe('Cart Item', () => {
+    describe('item count input', () => {
+        it('increment button adds 1', async () => {
+            const user = userEvent.setup();
+
+            render(<CartItem></CartItem>);
+            const button = screen.getByRole('button', { name: /\+/i });
+            await user.click(button);
+
+            expect(screen.getByLabelText('amount').value).toBe("1");
+
+        });
+    });
+
+});
