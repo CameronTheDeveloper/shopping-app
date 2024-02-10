8a70ce9c030fcd63dae27bacdfd589b09711b146
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-09 23:04:19 -0800

    Add CartItem initial value test

--- src/tests/CartItem.test.jsx
+++ src/tests/CartItem.test.jsx
@@ -4,15 +4,22 @@ import CartItem from '../Components/CartItem/CartItem';
 
 describe('Cart Item', () => {
     describe('item count input', () => {
-        it('increment button adds 1', async () => {
-            const user = userEvent.setup();
 
+        let user;
+
+        beforeEach(() => {
+            user = userEvent.setup();
             render(<CartItem></CartItem>);
+        });
+
+        it('displays initial value of 0', () => {
+            expect(screen.getByLabelText('amount').value).toBe("0");
+        });
+
+        it('increment button adds 1', async () => {
             const button = screen.getByRole('button', { name: /\+/i });
             await user.click(button);
-
             expect(screen.getByLabelText('amount').value).toBe("1");
-
         });
     });
 
