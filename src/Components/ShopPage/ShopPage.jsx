38957f7e53e9dcf7cd3d23ca3e601198f2853dd6
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-17 22:24:37 -0800

    Add shop data states with loading & error display

--- src/Components/ShopPage/ShopPage.jsx
+++ src/Components/ShopPage/ShopPage.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+import { getShopData } from "../get-shop-data";
 import CartItem from "../CartItem/CartItem";
+import styles from "./ShopPage.module.css";
 
 function ShopPage() {
+    const [shopData, setShopData] = useState(null);
+    const [shopDataLoading, setShopDataLoading] = useState(true);
+    const [shopDataError, setShopDataError] = useState(false);
+
+    if (shopDataLoading) return <h4>Loading shop products, please stand by...</h4>;
+    if (shopDataError) return <h3>A network error ocurred while retrieving shop products</h3>;
+
     return (
         <>
 
