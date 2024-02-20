5dab2271d5649f6d82caa5c30de0c9d9f562cf76
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-20 06:29:17 -0800

    Add fetch mock & shop data fetching test

--- src/tests/get-shop-data.test.js
+++ src/tests/get-shop-data.test.js
@@ -1,1 +1,23 @@
 import { getShopData } from "../Components/get-shop-data";
+import { vi } from "vitest";
+
+global.fetch = vi.fn(() => {
+    return (Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve([{ data: 'mockData' }])
+    }));
+});
+
+describe('getShopData()', () => {
+    it('fetches shop data successfully', async () => {
+        const setShopDataMock = vi.fn();
+        const setErrorMock = vi.fn();
+        const setLoadingMock = vi.fn();
+
+        await getShopData(setShopDataMock, setErrorMock, setLoadingMock);
+
+        expect(setShopDataMock).toHaveBeenCalledWith([{ data: 'mockData' }]);
+        expect(setErrorMock).not.toHaveBeenCalled();
+        expect(setLoadingMock).toHaveBeenCalledWith(false);
+    });
+});
