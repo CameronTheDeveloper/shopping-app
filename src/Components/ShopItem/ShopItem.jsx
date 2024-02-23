b8fd45543d1ffaf5192a338641c1022c64ad2648
CameronTheDeveloper <Cameronposey@outlook.com>
2024-02-23 03:58:53 -0800

    Format ShopItem price display decimal places to 2

--- src/Components/ShopItem/ShopItem.jsx
+++ src/Components/ShopItem/ShopItem.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { formattedPrecision } from "../format-decimal-precision";
 import PropTypes from "prop-types";
 import styles from "./ShopItem.module.css";
 
 
 function ShopItem({ title, price }) {
     const [amount, setAmount] = useState(0);
+    const formattedPrice = formattedPrecision(price, 2);
 
     const changeAmount = (value) => {
         setAmount(+value);
@@ -13,7 +15,8 @@ function ShopItem({ title, price }) {
     return (
         <div className={styles.ShopItem}>
             <h1>{title}</h1>
-            <h2>{`$${price} USD`}</h2>
+            <h2>{`$${formattedPrice} USD`}</h2>
+            {/* <h2>{`$${price} USD`}</h2> */}
             <div>
                 <button onClick={() => changeAmount(amount + 1)}>+</button>
                 <button disabled={amount <= 0} onClick={() => changeAmount(amount - 1)}>-</button>
