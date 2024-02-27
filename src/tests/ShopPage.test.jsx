a4d63fb7f73ee0102048803e6388f957627e4482
Cameron Posey <cameronposey@outlook.com>
2024-02-27 04:17:02 -0800

    Add ShopPage item display test & mock components

--- src/tests/ShopPage.test.jsx
+++ src/tests/ShopPage.test.jsx
@@ -1,16 +1,46 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
-import { vi } from "vitest";
+import { expect, vi } from "vitest";
 import ShopPage from "../Components/ShopPage/ShopPage";
 import { getShopData } from "../Components/get-shop-data";
+import { act } from "react-dom/test-utils";
 
+global.fetch = vi.fn(() => {
+    return (Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve("mockData")
+    }));
+});
 
-vi.mock("../Components/get-shop-data");
+vi.mock("../Components/get-shop-data", () => ({getShopData: vi.fn()}));
+
+vi.mock("../Components/ShopItemContainer/ShopItemContainer", () => ({
+    default: ({shopData}) => {
+        return (
+            <div>{shopData}</div>
+        )
+       
+    }
+}));
 
 describe('ShopPage', () => {
 
     it('displays loading message before shop data is fetched', () => {
-        render(<ShopPage></ShopPage>);
+        render(<ShopPage></ShopPage>);//Problem: ShopPage needs to be rendered here without calling effect
         const loadMessage = screen.getByRole("heading", { name: /loading shop products/i });
         expect(loadMessage).toBeInTheDocument();
     });
+
+    it('displays item container when data is fetched', async () => {
+        getShopData.mockImplementationOnce((setData, setLoading, setError) => {
+            setData("mockData");
+            setLoading(false);
+        })
+
+        await act(async () => {
+            render(<ShopPage></ShopPage>)
+        })
+        const shopData = screen.getByText(/mockData/i)
+        expect(shopData).toBeInTheDocument();
+    })
 });
