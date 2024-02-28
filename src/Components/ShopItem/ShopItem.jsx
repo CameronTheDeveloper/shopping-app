6d8ea47559fa59ea094a510d4c777ac1b988ddf4
Cameron Posey <cameronposey@outlook.com>
2024-02-28 02:53:58 -0800

    Reduce SHopItem title element sizes

--- src/Components/ShopItem/ShopItem.jsx
+++ src/Components/ShopItem/ShopItem.jsx
@@ -27,8 +27,8 @@ function ShopItem({ title, price }) {
 
     return (
         <div className={styles.ShopItem}>
-            <h1>{title}</h1>
-            <h2>{`$${formattedPrice} USD`}</h2>
+            <h2>{title}</h2>
+            <h3>{`$${formattedPrice} USD`}</h3>
             <div>
                 <button onClick={() => changeAmount(amount + 1)}>+</button>
                 <button disabled={amount <= 0} onClick={() => changeAmount(amount - 1)}>-</button>
