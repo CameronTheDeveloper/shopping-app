1ec6af21343abaa5d6bda6f4c9b6bbc293400a4f
Cameron Posey <cameronposey@outlook.com>
2024-02-28 06:06:11 -0800

    Format ShoppingCart total to two decimal places

--- src/Components/ShoppingCart/ShoppingCart.jsx
+++ src/Components/ShoppingCart/ShoppingCart.jsx
@@ -2,15 +2,17 @@ import PropTypes from "prop-types";
 import Icon from "@mdi/react";
 import { mdiCartVariant } from "@mdi/js";
 import styles from "./ShoppingCart.module.css";
+import { formattedPrecision } from "../format-decimal-precision";
 
 function ShoppingCart({ cartTotal = 0 }) {
+    const formattedTotal = formattedPrecision(cartTotal);
     return (
         <button className={styles.cart}>
             <Icon path={mdiCartVariant}
                 size={1}
                 title={"Shopping Cart"}
             ></Icon>
-            <div>{`$ ${cartTotal}`}</div>
+            <div>{`$ ${formattedTotal}`}</div>
             <div>Checkout</div>
         </button>
     );
