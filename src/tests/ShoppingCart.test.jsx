7a52aa92e82b046f3dbbb708c4a654237cf91dcc
Cameron Posey <cameronposey@outlook.com>
2024-02-27 22:03:04 -0800

    Add ShoppingCart button render test

--- src/tests/ShoppingCart.test.jsx
+++ src/tests/ShoppingCart.test.jsx
@@ -1,1 +1,13 @@
+import { describe, expect, it } from "vitest";
 import ShoppingCart from "../Components/ShoppingCart/ShoppingCart";
+import { render, screen } from "@testing-library/react";
+
+describe("ShoppingCart", () => {
+
+    it("renders shopping cart button to user", () => {
+        render(<ShoppingCart></ShoppingCart>);
+
+        const cartButton = screen.getByRole("button", {name: /Shopping Cart/i});
+        expect(cartButton).toBeInTheDocument();
+    })
+})
